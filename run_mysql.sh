922a965eeb94a3bf7e8c9aa1e7a1e3af765575a1
Enz0Z <48935178+Enz0Z@users.noreply.github.com>
2023-05-01 22:05:35 +0200

    Update run_mysql.sh

--- run_mysql.sh
+++ run_mysql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.31-linux-glibc2.12-x86_64.tar.xz
-tar xf mysql-8.0.31-linux-glibc2.12-x86_64.tar.xz
-chown -R 700 mysql-8.0.31-linux-glibc2.12-x86_64
-cd mysql-8.0.31-linux-glibc2.12-x86_64/bin
-apt-get install libnuma-dev -y
+wget https://cdn.mysql.com/archives/mysql-8.0/mysql-8.0.32-linux-glibc2.12-x86_64.tar.xz
+tar xf mysql-8.0.32-linux-glibc2.12-x86_64.tar.xz
+chown -R 700 mysql-8.0.32-linux-glibc2.12-x86_64
+cd mysql-8.0.32-linux-glibc2.12-x86_64/bin
+apt-get install libnuma-dev libaio1 -y
 ./mysqld --initialize
 # ./mysqld --console --user=root <-- NOT RECOMMENDED
