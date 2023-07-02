25cf47eaebe019e868f0b22af3313e47ddf407a7
Enz0Z <48935178+Enz0Z@users.noreply.github.com>
2023-07-02 03:28:11 +0200

    Create ftp_server.js

---
+++ ftp_server.js
@@ -0,0 +1,45 @@
+const ftpd = require('ftpd');
+const PORT = 21;
+const USERS = [];
+
+const server = new ftpd.FtpServer('0.0.0.0', {
+    getInitialCwd: function () {
+        return '/';
+    },
+    getRoot: function (connection) {
+        const user = USERS.find(user => user.username === connection.username);
+
+        return user.directory;
+    },
+    pasvPortRangeStart: 1024,
+    pasvPortRangeEnd: 65535,
+    useWriteFile: false,
+    useReadFile: false
+});
+
+server.on('client:connected', function (connection) {
+    var username = null;
+
+    connection.on('command:user', function (user, success, failure) {
+        if (user) {
+            username = user;
+            success();
+        } else {
+            failure();
+        }
+    });
+
+    connection.on('command:pass', function (pass, success, failure) {
+        const user = USERS.find(user => user.username === username && user.password === pass);
+
+        if (pass && user) {
+            success(username);
+        } else {
+            failure();
+        }
+    });
+});
+
+server.debugging = -1;
+
+server.listen(PORT);
